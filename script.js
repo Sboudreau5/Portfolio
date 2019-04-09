a66bcf1677c1a3ca54e39b5db344edd77431d89b
Shawn Boudreau <46534718+Sboudreau5@users.noreply.github.com>
2019-04-08 20:00:15 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ document.getElementById('Hamburger-Menu').addEventListener('click',
 	document.querySelector('.side-nav').style.width = '20em'
     document.querySelector('.hamburgerimenu').style.display = 'none'
     document.querySelector('.exitmenu').style.display = 'flex'
-     document.querySelector('ul').style.opacity = '1'
-    document.querySelector('.about-icon').style.opacity = '1'
-     document.querySelector('.home-icon').style.opacity = '1'
-    document.querySelector('.contact-icon').style.opacity = '1'
-    document.querySelector('.portfolio-icon').style.opacity = '1'
    document.querySelector('ul').style.display = 'block'
+document.querySelector('ul').style.opacity = '1'
     
     
 });
@@ -16,15 +12,14 @@ document.getElementById('Hamburger-Menu').addEventListener('click',
 
 document.getElementById('exitmenu').addEventListener('click',
 	function() {
+    document.querySelector('ul').style.display = 'none'
 	document.querySelector('.side-nav').style.width = '0em'
      document.querySelector('.hamburgerimenu').style.display = 'flex'
     document.querySelector('.exitmenu').style.display = 'none'
-    document.querySelector('ul').style.opacity = '0' 
-    document.querySelector('home-icon').style.opacity = '0'
- document.querySelector('about-icon').style.opacity = '0'
-     document.querySelector('.contact-icon').style.opacity = '0'
-    document.querySelector('.portfolio-icon').style.opacity = '0'
-     document.querySelector('ul').style.display = 'none'
+    document.querySelector('ul').style.display = 'none'
+    document.querySelector('ul').style.opacity = '0'
+    
+  
     
      
 });
